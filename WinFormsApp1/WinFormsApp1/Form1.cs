3a42d2f04310b1063fdf95f0f664657db3a54a30
kinga_rynkun <kinga_rynkun@DELL>
2026-03-06 21:22:56 +0100

    kod 2

--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -41,19 +41,19 @@ namespace WinFormsApp1
                 MessageBox.Show(ex.Message);
             }
         }
-        //private void Form1_Load(object sender, EventArgs e)
-        //{
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
-        //}
+        }
 
-        //private void label1_Click(object sender, EventArgs e)
-        //{
+        private void label1_Click(object sender, EventArgs e)
+        {
 
-        //}
+        }
 
-        //private void textBox1_TextChanged(object sender, EventArgs e)
-        //{
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
 
-        //}
+        }
     }
 }
