71ad41564b0f73daf71a9096530e09b58a321dd6
kinga_rynkun <kinga_rynkun@DELL>
2026-03-06 21:19:47 +0100

    Kod

--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -1,3 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Calculation;
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
@@ -6,20 +15,45 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+        calculate cal = new calculate();
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                int i = cal.Add(int.Parse(textBox2.Text), int.Parse(textBox1.Text));
+                textBox3.Text = i.ToString();
+            }
+            catch (Exception ex)
+            { 
+                MessageBox.Show(ex.Message);
+            }
         }
-
-        private void label1_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                int i = cal.Sub(int.Parse(textBox2.Text), int.Parse(textBox1.Text));
+                textBox3.Text = i.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
+        //}
 
-        }
+        //private void label1_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //private void textBox1_TextChanged(object sender, EventArgs e)
+        //{
+
+        //}
     }
 }
