7e37461b79310dd0ef643362e9addd08799389e2
kinga_rynkun <kinga_rynkun@DELL>
2026-03-19 14:40:46 +0100

    Dodanie zapisu do pliku

--- lab 3/lab3/lab3/Form1.cs
+++ lab 3/lab3/lab3/Form1.cs
@@ -24,11 +24,43 @@ namespace lab3
         {
 
         }
-
+        private void ExportToCSV(DataGridView dataGridView, string filePath)
+        {
+            // Tworzenie nag³ówka pliku CSV
+            string csvContent = "Column1,Column2,Column3" + Environment.NewLine;
+            // Dodawanie danych z DataGridView
+            foreach (DataGridViewRow row in dataGridView.Rows)
+            {
+                // Pomijaj wiersze niemieszcz¹ce siê w DataGridView (np. wiersz zaznaczania)
+                if (!row.IsNewRow)
+                {
+                    // Dodaj kolejne wartoœci w wierszu, oddzielone przecinkami
+                    csvContent += string.Join(",", Array.ConvertAll(row.Cells.Cast<DataGridViewCell>()
+                    .ToArray(), c => c.Value)) + Environment.NewLine;
+                }
+            }
+            // Zapisanie zawartoœci do pliku CSV
+            File.WriteAllText(filePath, csvContent);
+        }
         private void button3_Click(object sender, EventArgs e)
         {
 
+
+            // Wyœwietlanie okna dialogowego wyboru lokalizacji zapisu
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Pliki CSV (*.csv)|*.csv|Wszystkie pliki (*.*)|*.*";
+            saveFileDialog1.Title = "Wybierz lokalizacjê zapisu pliku CSV";
+            saveFileDialog1.ShowDialog();
+            // Jeœli u¿ytkownik wybierze lokalizacjê i zatwierdzi, zapisz plik CSV
+            if (saveFileDialog1.FileName != "")
+            {
+                // U¿yj metody ExportToCSV i podaj obiekt DataGridView oraz œcie¿kê do pliku CSV
+                ExportToCSV(dataGridView1, saveFileDialog1.FileName);
+            }
+
         }
+
+
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -42,3 +74,4 @@ namespace lab3
         }
     }
 }
+
