ca6db783ac01654e2fc4e56b6b274eae2029b1fb
kinga_rynkun <kinga_rynkun@DELL>
2026-04-22 15:51:14 +0200

    Przycisk load

--- lab 4/WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ lab 4/WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -30,14 +30,20 @@
         {
             button1 = new Button();
             pictureBox1 = new PictureBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(36, 332);
+            button1.Location = new Point(12, 377);
             button1.Name = "button1";
-            button1.Size = new Size(226, 97);
+            button1.Size = new Size(250, 61);
             button1.TabIndex = 0;
             button1.Text = "Load";
             button1.UseVisualStyleBackColor = true;
@@ -52,22 +58,98 @@
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
             // 
+            // button2
+            // 
+            button2.Location = new Point(12, 104);
+            button2.Name = "button2";
+            button2.Size = new Size(250, 61);
+            button2.TabIndex = 2;
+            button2.Text = "Rotate";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(12, 171);
+            button3.Name = "button3";
+            button3.Size = new Size(250, 61);
+            button3.TabIndex = 3;
+            button3.Text = "Invert Colors";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(12, 238);
+            button4.Name = "button4";
+            button4.Size = new Size(250, 61);
+            button4.TabIndex = 4;
+            button4.Text = "Upside Down";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(12, 14);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(46, 24);
+            radioButton1.TabIndex = 5;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "90";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(12, 44);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(54, 24);
+            radioButton2.TabIndex = 6;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "180";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(12, 74);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(54, 24);
+            radioButton3.TabIndex = 7;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "270";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(pictureBox1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
         private PictureBox pictureBox1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
     }
 }
--- lab 4/WinFormsApp1/WinFormsApp1/Form1.cs
+++ lab 4/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -4,15 +4,38 @@ namespace WinFormsApp1
     {
         public Form1()
         {
+
             InitializeComponent();
         }
+        private Bitmap loadedImage;
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Bitmap (*.bmp)|*.bmp";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                loadedImage = new Bitmap(dialog.FileName);
+                pictureBox1.Image = loadedImage;
+            }
+        }
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
         {
 
         }
