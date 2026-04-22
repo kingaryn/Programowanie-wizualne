cfc935833005ed093635261f97c38a0cfc4790d3
kinga_rynkun <kinga_rynkun@DELL>
2026-04-22 15:33:37 +0200

    Main

--- lab 4/WinFormsApp1/WinFormsApp1/Form1.cs
+++ lab 4/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -6,5 +6,15 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
